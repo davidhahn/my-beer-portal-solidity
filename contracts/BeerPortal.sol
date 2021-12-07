6bcacfe733797a39ebe11df7725f4b597f5ccbf0
David Hahn <davidhahn@gmail.com>
2021-12-06 20:19:40 -0500

    initial commit

---
+++ contracts/BeerPortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract BeerPortal {
+    constructor() {
+        console.log(unicode"🍺🍻, I am a contract and I am smart");
+    }
+}
