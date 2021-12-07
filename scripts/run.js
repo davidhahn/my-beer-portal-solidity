efced432998a0c46a7218616437d635a574219ab
David Hahn <davidhahn@gmail.com>
2021-12-06 21:25:57 -0500

    modify run.js to beer me

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,24 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const beerContractFactory = await hre.ethers.getContractFactory('BeerPortal');
   const beerContract = await beerContractFactory.deploy();
   await beerContract.deployed();
+
   console.log("Contract deployed to:", beerContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let beerCount;
+  beerCount = await beerContract.getTotalBeer();
+
+  let beerTxn = await beerContract.receiveBeer();
+  await beerTxn.wait();
+
+  beerCount = await beerContract.getTotalBeer();
+
+  beerTxn = await beerContract.connect(randomPerson).receiveBeer();
+  await beerTxn.wait();
+
+  beerCount = await beerContract.getTotalBeer();
 };
 
 const runMain = async () => {
