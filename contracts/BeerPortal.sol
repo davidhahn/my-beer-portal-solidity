1685319e0bae56376dc939163a232a554395f151
David Hahn <davidhahn@gmail.com>
2021-12-06 21:08:16 -0500

    update BeerPortal.sol

--- contracts/BeerPortal.sol
+++ contracts/BeerPortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract BeerPortal {
+    uint256 totalBeer;
+
     constructor() {
         console.log(unicode"🍺🍻, I am a contract and I am smart");
+    }
+
+    function receiveBeer() public {
+        totalBeer += 1;
+        console.log("%s has given you beer", msg.sender);
+    }
+
+    function getTotalBeer() public view returns (uint256) {
+        console.log("We have %d total beer", totalBeer);
+        return totalBeer;
     }
 }
